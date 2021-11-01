0ebe356a26f33ac352ec56accc0a9cb70f6cd60b
xuan.guo@atomdatatech.com <xuan.guo@atomdatatech.com>
2021-11-01 21:26:11 +0800

    update demo usage

--- main.cpp
+++ main.cpp
@@ -12,8 +12,13 @@ struct MyOpts
 
 int main(int argc, char** argv)
 {
-    auto flag = flags<MyOpts>("test", "1.0.0", { { "-a", &MyOpts::stringOpt }, { "-b", &MyOpts::intOpt } });
+    auto flag = flags<MyOpts>("test", "1.0.0", { { "-a", &MyOpts::stringOpt, "message a" }, { "-b", &MyOpts::intOpt, "message b" } });
     auto myopts = flag.parse(argc, argv);
+    if (argc == 1)
+    {
+        std::cout << flag.usage();
+        return 0;
+    }
     std::cout << "stringOpt = " << myopts.stringOpt << endl;
     std::cout << "intOpt = " << myopts.intOpt << endl;
 }
