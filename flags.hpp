0ee379252e966da8743f74951d3a71463343c77c
xuan.guo@atomdatatech.com <xuan.guo@atomdatatech.com>
2021-11-02 11:10:35 +0800

    rework

--- flags.hpp
+++ flags.hpp
@@ -1,11 +1,18 @@
+//  (C) Copyright 2021 kizi@qq.com
+//  https://github.com/kkzi/flags.git
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #pragma once
 
 #include <functional>
+#include <iostream>
 #include <map>
 #include <memory>
 #include <sstream>
 #include <string>
 #include <string_view>
+#include <tuple>
 #include <variant>
 #include <vector>
 
@@ -14,57 +21,47 @@ class flags : T
 {
 private:
     using property_t = std::variant<std::string T::*, int T::*, double T::*>;
-    using callback_t = std::function<void(const std::string&)>;
-    struct option_t
-    {
-        std::string name_;
-        property_t prop_;
-        std::string desc_;
-        callback_t callback_;
-    };
+    using callback_t = std::function<void(const std::string &)>;
+    using option_t = std::tuple<std::string, std::string, property_t>;
+    using option_parser_t = std::tuple<std::string, callback_t>;
 
 public:
-    flags(std::string app, std::string version, const std::initializer_list<option_t>& opts)
+    flags(std::string app, std::string version, const std::initializer_list<option_t> &opts)
         : app_(std::move(app))
         , ver_(std::move(version))
     {
-        for (const auto& o : opts)
+        auto parse = [this](const property_t &p, const std::string &val) {
+            std::visit(
+                [this, val](auto &&arg) {
+                    std::stringstream ss;
+                    ss << val;
+                    ss >> this->*arg;
+                },
+                p);
+        };
+
+        for (const auto &[name, desc, prop] : opts)
         {
-            opts_[o.name_] = option_t{
-                o.name_,
-                o.prop_,
-                o.desc_,
-                [this, p = o.prop_](const std::string& val) {
-                    std::visit([this, val](auto&& arg) {
-                        std::stringstream ss;
-                        ss << val;
-                        ss >> this->*arg;
-                    },
-                    p);
-                }
-            };
+            register_option_parser(name, desc, [parse, prop](auto &&val) {
+                parse(prop, val);
+            });
         }
 
-        if (opts_.count("-help") == 0)
-        {
-            option_t help_opt;
-            help_opt.name_ = "-help";
-            help_opt.desc_ = "show this help message";
-            help_opt.callback_ = [this](auto&&) { std::cout << usage() << std::endl; };
-            opts_["-help"] = help_opt;
-        }
+        register_option_parser("--help", "show this help message", [this](auto &&) {
+            std::cout << usage();
+            exit(0);
+        });
 
-        if (!app_.empty() && !ver_.empty() && opts_.count("-version") == 0)
+        if (!app_.empty() && !ver_.empty())
         {
-            option_t ver_opt;
-            ver_opt.name_ = "-version";
-            ver_opt.desc_ = "show app version";
-            ver_opt.callback_ = [this](auto&&) { std::cout << app_ << " " << ver_ << std::endl; };
-            opts_["-version"] = ver_opt;
+            register_option_parser("--version", "show app version", [this](auto &&) {
+                std::cout << app_ << " " << ver_ << std::endl;
+                exit(0);
+            });
         }
     }
 
-    flags(const std::initializer_list<option_t>& opts)
+    flags(const std::initializer_list<option_t> &opts)
         : flags("", "", opts)
     {
     }
@@ -72,7 +69,7 @@ public:
     ~flags() = default;
 
 public:
-    T parse(int argc, char** argv)
+    T parse(int argc, char **argv)
     {
         std::vector<std::string_view> vargv(argv, argv + argc);
         for (auto idx = 1; idx < argc; ++idx)
@@ -91,13 +88,18 @@ public:
                 val = line.substr(equal_pos + 1);
             }
 
-            if (opts_.count(key) > 0)
+            if (parsers_.count(key) > 0)
             {
-                opts_.at(key).callback_(val);
+                std::get<1>(parsers_.at(key))(val);
+            }
+            else
+            {
+                std::cout << "invalid argument " << key << std::endl;
+                exit(0);
             }
         }
 
-        return static_cast<T>(*this);
+        return *this;
     }
 
     std::string usage()
@@ -107,22 +109,30 @@ public:
         {
             ss << app_ << " " << ver_ << std::endl;
         }
-        for (auto&& [name, opt] : opts_)
+        for (auto &&[name, tp] : parsers_)
         {
-            ss << "  " << name << ": " << opt.desc_ << std::endl;
+            ss << "  " << name << ": " << std::get<0>(tp) << std::endl;
         }
-        ss << std::endl;
         return ss.str();
     }
 
 private:
-    flags(const flags&) = delete;
-    flags(flags&&) = delete;
-    flags& operator=(const flags&) = delete;
-    flags& operator=(flags&&) = delete;
+    void register_option_parser(const std::string &name, const std::string &desc, const callback_t &func)
+    {
+        if (parsers_.count(name) == 0 && func != nullptr)
+        {
+            parsers_[name] = option_parser_t{ desc, func };
+        }
+    }
+
+private:
+    flags(const flags &) = delete;
+    flags(flags &&) = delete;
+    flags &operator=(const flags &) = delete;
+    flags &operator=(flags &&) = delete;
 
 private:
     std::string app_;
     std::string ver_;
-    std::map<std::string, option_t> opts_;
+    std::map<std::string, option_parser_t> parsers_;
 };
--- main.cpp
+++ main.cpp
@@ -7,12 +7,11 @@ using namespace std;
 struct MyOpts
 {
     string stringOpt{};
-    int intOpt{ 100 };
 };
 
 int main(int argc, char** argv)
 {
-    auto flag = flags<MyOpts>("test", "1.0.0", { { "-a", &MyOpts::stringOpt, "message a" }, { "-b", &MyOpts::intOpt, "message b" } });
+    auto flag = flags<MyOpts>("test", "1.0.0", { { "-a", "message a", &MyOpts::stringOpt } });
     auto myopts = flag.parse(argc, argv);
     if (argc == 1)
     {
@@ -20,5 +19,5 @@ int main(int argc, char** argv)
         return 0;
     }
     std::cout << "stringOpt = " << myopts.stringOpt << endl;
-    std::cout << "intOpt = " << myopts.intOpt << endl;
+    //std::cout << "intOpt = " << myopts.intOpt << endl;
 }
